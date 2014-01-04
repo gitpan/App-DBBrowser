746eaac1e9d63545033550be428b4ce1b73ce66d
Matthäus Kiem <cuer2s@gmail.com>
2014-01-04 11:05:14 +0000

    Import of KUERBIS/App-DBBrowser-0.001 from CPAN.
    gitpan-cpan-distribution: App-DBBrowser
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         KUERBIS/App-DBBrowser-0.001.tar.gz
    gitpan-cpan-author:       KUERBIS
    gitpan-cpan-maturity:     released

---
+++ lib/App/DBBrowser.pm
@@ -0,0 +1,58 @@
+package App::DBBrowser;
+
+use warnings;
+use strict;
+use 5.10.1;
+
+our $VERSION = '0.001';
+
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::DBBrowser - Search and read in SQLite/MySQL/PostgreSQL databases.
+
+=head1 VERSION
+
+Version 0.001
+
+=head1 SYNOPSIS
+
+db-browser
+
+db-browser -h|--help
+
+    -h|--help    show the config menu (which offers also a help text).
+
+=head2 SQLite:
+
+db-browser [-s|--search] [directories to be searched]
+
+    If no directories are passed the home directory is searched for SQLite databases.
+
+    -s|--search    new search of SQLite databases (don't use cached data).
+
+=head1 DESCRIPTION
+
+Search and read in SQLite/MySQL/PostgreSQL databases.
+
+For further information see L<db-browser>
+
+=head1 AUTHOR
+
+Matthäus Kiem <cuer2s@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2012-2014 Matthäus Kiem.
+
+This is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
---
+++ xt/spelling.t
@@ -0,0 +1,26 @@
+use 5.010001;
+use strict;
+use warnings;
+use Test::More;
+
+use Test::Spelling;
+
+
+add_stopwords( <DATA> );
+
+all_pod_files_spelling_ok( 'bin', 'lib' );
+
+
+
+__DATA__
+BNRY
+Colwidth
+Ctrl
+Kiem
+Lk
+Matthäus
+ProgressBar
+Sssc
+Tabwidth
+compat
+repexp
