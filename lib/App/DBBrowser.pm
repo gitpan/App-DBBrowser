53e934c4b5f4a40e6b30ad7f505b630668b8ae98
Matthäus Kiem <cuer2s@gmail.com>
2014-01-04 13:41:03 +0000

    Import of KUERBIS/App-DBBrowser-0.003 from CPAN.
    gitpan-cpan-distribution: App-DBBrowser
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         KUERBIS/App-DBBrowser-0.003.tar.gz
    gitpan-cpan-author:       KUERBIS
    gitpan-cpan-maturity:     released

--- lib/App/DBBrowser.pm
+++ lib/App/DBBrowser.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use 5.10.1;
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 
 1;
@@ -21,7 +21,7 @@ App::DBBrowser - Search and read in SQLite/MySQL/PostgreSQL databases.
 
 =head1 VERSION
 
-Version 0.002
+Version 0.003
 
 =head1 SYNOPSIS
 
